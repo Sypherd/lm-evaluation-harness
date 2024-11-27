c7e90fbe6768ed986e1b4c80d7317021faa4e4ac
Sypherd <sypherdchris@gmail.com>
2024-11-27 16:12:15 +0000

    Fix minor bugs for tot

--- lm_eval/models/vllm_causallms_tot.py
+++ lm_eval/models/vllm_causallms_tot.py
@@ -1,14 +1,15 @@
 import copy
 import itertools
-import numpy as np
 import re
-
 from typing import List
 
+import numpy as np
+from vllm.outputs import RequestOutput
+
 from lm_eval.api.instance import Instance
+from lm_eval.api.registry import register_model
 from lm_eval.models.vllm_causallms import VLLM
 from lm_eval.prompts import PROMPT_REGISTRY
-from lm_eval.api.registry import register_model
 from lm_eval.utils import eval_logger
 
 
@@ -19,25 +20,33 @@ class VLLM_ToT(VLLM):
         self.in_tokens_count = 0
         self.out_tokens_count = 0
         self.value_cache = {}
+        self.task = "gsm8k"
 
     def get_samples(self, context, y, n_samples, max_gen_toks, until):
-        prompt = PROMPT_REGISTRY["tree-of-thought"]["cot-prompt"].format(input=context) + y
+        prompt = (
+            PROMPT_REGISTRY["tree-of-thought"][self.task]["cot-prompt"].format(
+                input=context
+            )
+            + y
+        )
         context_encoding = self.prepare_context(prompt, max_gen_toks)
 
-        #TODO check when max_gen_toks is None
+        # TODO check when max_gen_toks is None
         samples = self._model_generate(
             requests=context_encoding,
             generate=True,
             max_tokens=max_gen_toks,
             stop=until,
-            n=n_samples
+            n=n_samples,
         )
         self.count_out_tokens(samples)
 
-        return [y + _ for _ in samples]
+        return [y + s.text for s in samples[0].outputs]
 
     def get_proposals(self, context, y, max_gen_toks, until):
-        propose_prompt = PROMPT_REGISTRY["tree-of-thought"]["propose-prompt"].format(input=context)
+        propose_prompt = PROMPT_REGISTRY["tree-of-thought"][self.task][
+            "propose-prompt"
+        ].format(input=context)
         context_encoding = self.prepare_context(propose_prompt, max_gen_toks)
 
         proposals = self._model_generate(
@@ -45,24 +54,28 @@ class VLLM_ToT(VLLM):
             generate=True,
             max_tokens=max_gen_toks,
             stop=until,
-            n=1
+            # n=1,
         )
         self.count_out_tokens(proposals)
-        proposals = proposals[0].split('\n')
+        proposals = proposals[0].outputs[0].text.split("\n")
 
-        return [y + _ + '\n' for _ in proposals]
+        return [y + _ + "\n" for _ in proposals]
 
     @staticmethod
-    def value_outputs_unwrap(y: str, value_outputs: list) -> float:
-        if len(y.strip().split('\n')) == 4 and 'answer' not in y.lower():
+    def value_outputs_unwrap(y: str, value_outputs: list[RequestOutput]) -> float:
+        if len(y.strip().split("\n")) == 4 and "answer" not in y.lower():
             return 0
-        value_names = [_.split('\n')[-1] for _ in value_outputs]
-        value_map = {'impossible': 0.001, 'likely': 1, 'sure': 20}  # TODO: ad hoc
-        value = sum(value * value_names.count(name) for name, value in value_map.items())
+        value_names = [vo.text.split("\n")[-1] for vo in value_outputs[0].outputs]
+        value_map = {"impossible": 0.001, "likely": 1, "sure": 20}  # TODO: ad hoc
+        value = sum(
+            value * value_names.count(name) for name, value in value_map.items()
+        )  # brittle check
         return value
 
     def get_value(self, context, y, n_samples, max_gen_toks, until, cache_value=True):
-        value_prompt = PROMPT_REGISTRY["tree-of-thought"]["value-prompt"].format(input=context)
+        value_prompt = PROMPT_REGISTRY["tree-of-thought"][self.task][
+            "value-prompt"
+        ].format(input=context)
 
         if cache_value and value_prompt in self.value_cache:
             return self.value_cache[value_prompt]
@@ -74,7 +87,7 @@ class VLLM_ToT(VLLM):
             generate=True,
             max_tokens=max_gen_toks,
             stop=until,
-            n=n_samples
+            n=n_samples,
         )
         self.count_out_tokens(value_outputs)
         value = self.value_outputs_unwrap(y, value_outputs)
@@ -90,7 +103,9 @@ class VLLM_ToT(VLLM):
             if y in local_value_cache:  # avoid duplicate candidates
                 value = 0
             else:
-                value = self.get_value(context, y, n_samples, max_gen_toks, until, cache_value=cache_value)
+                value = self.get_value(
+                    context, y, n_samples, max_gen_toks, until, cache_value=cache_value
+                )
                 local_value_cache[y] = value
             values.append(value)
         return values
@@ -99,18 +114,20 @@ class VLLM_ToT(VLLM):
     def vote_outputs_unwrap(vote_outputs: list, n_candidates: int) -> list:
         vote_results = [0] * n_candidates
         for vote_output in vote_outputs:
-            pattern = r".*best choice is .*(\d+).*"
+            pattern = r".*best choice is .*(\d+).*"  # brittle
             match = re.match(pattern, vote_output, re.DOTALL)
             if match:
                 vote = int(match.groups()[0]) - 1
                 if vote in range(n_candidates):
                     vote_results[vote] += 1
             else:
-                print(f'vote no match: {[vote_output]}')
+                print(f"vote no match: {[vote_output]}")
         return vote_results
 
     def get_votes(self, context, ys, n_samples, max_gen_toks, until):
-        vote_prompt = PROMPT_REGISTRY["tree-of-thought"]["vote-prompt"].format(input=context)
+        vote_prompt = PROMPT_REGISTRY["tree-of-thought"][self.task][
+            "vote-prompt"
+        ].format(input=context)
         context_encoding = self.prepare_context(vote_prompt, max_gen_toks)
 
         vote_outputs = self._model_generate(
@@ -118,7 +135,7 @@ class VLLM_ToT(VLLM):
             generate=True,
             max_tokens=max_gen_toks,
             stop=until,
-            n=n_samples
+            n=n_samples,
         )
         self.count_out_tokens(vote_outputs)
         values = self.vote_outputs_unwrap(vote_outputs, len(ys))
@@ -132,18 +149,21 @@ class VLLM_ToT(VLLM):
         # set the max length in tokens of inputs ("context_enc")
         # max len for inputs = max length, minus room to generate the max new tokens
         max_ctx_len = self.max_length - max_gen_toks
-        context_encoding = [x[-max_ctx_len:] for x in context_encoding]
-        self.in_tokens_count += len(context_encoding)
+        if isinstance(context_encoding[0], list):
+            context_encoding = [x[:max_ctx_len] for x in context_encoding]
+            self.in_tokens_count += sum(len(x) for x in context_encoding)
+        else:
+            context_encoding = context_encoding[:max_ctx_len]
+            self.in_tokens_count += len(context_encoding)
 
         return context_encoding
 
     def count_out_tokens(self, outputs):
-        for output in outputs:
-            self.out_tokens_count += len(self.tok_encode(
-                output, add_special_tokens=False
-            ))
+        for output in outputs[0].outputs:
+            self.out_tokens_count += len(output.token_ids)
 
     def generate_until(self, requests: list[Instance], to_print=True) -> List[str]:
+        self.task = requests[0].task_name.split("_")[0]
         yys = []
         for request in requests:
             context, gen_kwargs = request.args
@@ -174,57 +194,92 @@ class VLLM_ToT(VLLM):
             else:
                 max_gen_toks = self.max_gen_toks
 
-            ys = ['']  # current output candidates
+            ys = [""]  # current output candidates
             infos = []
             for step in range(gen_kwargs["steps"]):
                 # generation
-                if gen_kwargs["method_generate"] == 'sample':
+                if gen_kwargs["method_generate"] == "sample":
                     new_ys = [
-                        self.get_samples(context, y, gen_kwargs["n_generate_sample"], max_gen_toks, until)
+                        self.get_samples(
+                            context,
+                            y,
+                            gen_kwargs["n_generate_sample"],
+                            max_gen_toks,
+                            until,
+                        )
                         for y in ys
                     ]
-                elif gen_kwargs["method_generate"] == 'propose':
-                    new_ys = [self.get_proposals(context, y, max_gen_toks, until) for y in ys]
+                elif gen_kwargs["method_generate"] == "propose":
+                    new_ys = [
+                        self.get_proposals(context, y, max_gen_toks, until) for y in ys
+                    ]
                 else:
-                    raise ValueError(f"method {gen_kwargs['method_generate']} is not supported")
+                    raise ValueError(
+                        f"method {gen_kwargs['method_generate']} is not supported"
+                    )
                 new_ys = list(itertools.chain(*new_ys))
                 ids = list(range(len(new_ys)))
                 # evaluation
-                if gen_kwargs["method_evaluate"] == 'vote':
-                    values = self.get_votes(context, new_ys, gen_kwargs["n_evaluate_sample"], max_gen_toks, until)
-                elif gen_kwargs["method_evaluate"] == 'value':
-                    values = self.get_values(context, new_ys, gen_kwargs["n_evaluate_sample"], max_gen_toks, until)
+                if gen_kwargs["method_evaluate"] == "vote":
+                    values = self.get_votes(
+                        context,
+                        new_ys,
+                        gen_kwargs["n_evaluate_sample"],
+                        max_gen_toks,
+                        until,
+                    )
+                elif gen_kwargs["method_evaluate"] == "value":
+                    values = self.get_values(
+                        context,
+                        new_ys,
+                        gen_kwargs["n_evaluate_sample"],
+                        max_gen_toks,
+                        until,
+                    )
                 else:
-                    raise ValueError(f"method {gen_kwargs['method_evaluate']} is not supported")
+                    raise ValueError(
+                        f"method {gen_kwargs['method_evaluate']} is not supported"
+                    )
 
                 # selection
-                if gen_kwargs["method_select"] == 'sample':
+                if gen_kwargs["method_select"] == "sample":
                     ps = np.array(values) / sum(values)
-                    select_ids = np.random.choice(ids, size=gen_kwargs["n_select_sample"], p=ps).tolist()
-                elif gen_kwargs["method_select"] == 'greedy':
-                    select_ids = sorted(ids, key=lambda x: values[x], reverse=True)[:gen_kwargs["n_select_sample"]]
+                    select_ids = np.random.choice(
+                        ids, size=gen_kwargs["n_select_sample"], p=ps
+                    ).tolist()
+                elif gen_kwargs["method_select"] == "greedy":
+                    select_ids = sorted(ids, key=lambda x: values[x], reverse=True)[
+                        : gen_kwargs["n_select_sample"]
+                    ]
                 else:
-                    raise ValueError(f"method {gen_kwargs['method_select']} is not supported")
+                    raise ValueError(
+                        f"method {gen_kwargs['method_select']} is not supported"
+                    )
                 select_new_ys = [new_ys[select_id] for select_id in select_ids]
 
                 # log
-                sorted_new_ys, sorted_values = zip(*sorted(zip(new_ys, values), key=lambda x: x[1], reverse=True))
+                sorted_new_ys, sorted_values = zip(
+                    *sorted(zip(new_ys, values), key=lambda x: x[1], reverse=True)
+                )
                 eval_logger.debug(
-                    f'-- new_ys --: {sorted_new_ys}\n-- sol values --: {sorted_values}\n-- choices --: {select_new_ys}\n'
+                    f"-- new_ys --: {sorted_new_ys}\n-- sol values --: {sorted_values}\n-- choices --: {select_new_ys}\n"
                 )
 
                 infos.append(
                     {
-                        'step': step,
-                        'context': context,
-                        'ys': ys,
-                        'new_ys': new_ys,
-                        'values': values,
-                        'select_new_ys': select_new_ys
+                        "step": step,
+                        "context": context,
+                        "ys": ys,
+                        "new_ys": new_ys,
+                        "values": values,
+                        "select_new_ys": select_new_ys,
                     }
                 )
                 ys = select_new_ys
 
-            yys.append(ys[0])
+            yys.append(ys[0])  # See note
+            # In https://github.com/princeton-nlp/tree-of-thought-llm/blob/master/src/tot/prompts/game24.py,
+            # there's a final value prompt. Interestingly, there is no such valuation
+            # for the crosswords game. I think just taking the highest value here makes sense
 
         return yys
